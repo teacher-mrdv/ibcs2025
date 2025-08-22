fec3a3b670819f15d9bc562e52d36bf367f0c919
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-08-22 10:35:54 +0800

    Update Den2Bin.java

--- challenges/Den2Bin.java
+++ challenges/Den2Bin.java
@@ -9,6 +9,13 @@ public class Den2Bin
 	
 	public static void main (String[] args)
 	{
+		if(arguments.length < 1)
+		{	System.out.println("Error--no input");
+			System.exit(1);
+		}
+		// We will assume that the user kindly privided an integer...
+		int denary = Integer.parseInt(args[0]);
+		int temp = denary;
 		
 	}
 }
