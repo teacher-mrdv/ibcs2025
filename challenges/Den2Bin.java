3e6d6f6c4781e93a652bbb46b24017af574eba7e
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-08-22 10:42:52 +0800

    Update Den2Bin.java

--- challenges/Den2Bin.java
+++ challenges/Den2Bin.java
@@ -3,20 +3,22 @@
  * Command line version
  * IB CS 2025 mdv
  */
- 
 public class Den2Bin
 {
-	
 	public static void main (String[] args)
 	{
-		if(arguments.length < 1)
-		{	System.out.println("Error--no input");
+		if(args.length < 1)
+		{
+			System.out.println("Error--no input");
 			System.exit(1);
 		}
 		// We will assume that the user kindly privided an integer...
 		int denary = Integer.parseInt(args[0]);
 		int temp = denary;
+		String binary = "";
+		// the magic goes here...
 		
+		System.out.println(denary + " base 10 = " + binary + " base 2");
 	}
 }
 
