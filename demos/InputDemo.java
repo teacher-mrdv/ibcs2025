c41bd32a7157df8025b791dd183c0b7dc6d3c374
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-09-05 19:32:10 +0800

    Update InputDemo.java

--- demos/InputDemo.java
+++ demos/InputDemo.java
@@ -1,16 +1,20 @@
-
+/*
+ *  Torture for my dear grade 11s :)
+ */
+ 
 public class InputDemo
 {
 	
 	public static void main (String[] args)
 	{
 		if(args.length < 1)
-		{	System.out.println("Error-no input" + args.length);
+		{	System.out.println("Error-no input " + args.length);
 			System.exit(1);
 		}		
 		for(int i = 0; i < args.length; i++)
-		{	System.out.println(args[0]);
+		{	System.out.println(i + " " + args[i]);
 		}
+		System.out.println("Bye.");
 	}
 }
 
