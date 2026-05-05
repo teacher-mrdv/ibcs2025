2848ae141183d09cac9e216073f66bf8e3a815e7
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2026-05-05 18:25:38 +0800

    LinkedList HW answers

--- HL/ADTs/LinkedList.java
+++ HL/ADTs/LinkedList.java
@@ -1,16 +1,41 @@
 /*
  * LinkedList.java			note:	LL=linked list
  * This class defines the content and structure of a linked list
- * We haven't encapsulated/hidden its data yet the way we did it with the Node class - WIP
+ * Now fully encapsulated (+data hiding)
  * 
  */
 
 
 public class LinkedList {
 	// point of access to the linked list: HEAD attribute
-	Node head;
+	private Node head;
 	// attribute to keep track of the SIZE (number of nodes) of the linked list
-	int size = 0;
+	private int size = 0;
+
+	// custom constructor (it takes a parameter)
+	// Creates a linked list with a head (1 node)
+	public LinkedList(Node head) {
+		this.head = head;
+		size = 1;
+	}
+
+	// empty constructor (instantiates an EMPTY linked list
+	public LinkedList() {
+		head = null;
+		size = 0;
+	}
+	
+	public Node getHead() {
+		return head;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public int size() {
+		return size;
+	}
 
 	public boolean isEmpty() {
 		return head == null;
@@ -133,21 +158,101 @@ public class LinkedList {
 		size--;
 		return true;		// successful deletion!
 	}
-	
+
 	/* Challenge 1:
-	 * Construct a method to insert a new node AFTER
-	 *  the n-th node of a linked list, and add code to test it
-	 *  in the LinkedList class.
-	 */
-	
-	
+     * Construct a method to clone (copy) a linked list to a new LL
+     * Just like we did with arrays, you will need to create a new LL,
+     *  and copy one node at a time.
+     */
+    public LinkedList clone() {
+        LinkedList newList = new LinkedList(); // Always create the object
+
+        if (!this.isEmpty()) {
+            Node current = this.getHead();
+            while (current != null) {
+                // Create a new node with the same data
+                Node newNode = new Node(current.getData());
+                // Add it to the new list
+                newList.append(newNode);
+                // move to next node
+                current = current.getNext();
+            }
+        }
+        return newList;
+    }
 	
 	/* Challenge 2:
-	 * Construct a method to insert a new node BEFORE
-	 *  the n-th node of a linked list, and add code to test it
-	 *  in the LinkedList class.
+	 * Construct a method to insert a new node AFTER
+	 *  the n-th node of a linked list (place), and
+	 *  add code to test itn the LinkedList class.
 	 */
+	public void insertAfter(Node insert, int place) {
+		if(place < 1 || place > this.size()) {
+			System.out.println("ERROR - insertAfter " + place + " out of bounds!");
+			return;
+		}
+		if( place == 1 ) {
+			insert.setNext( head.getNext() );
+			head.setNext(insert);
+			size++;
+		} else {
+			int counter = 1;
+			Node temp = this.head;
+			while(counter < place) {
+				temp = temp.getNext();
+				counter++;
+			}
+			insert.setNext(temp.getNext());
+			temp.setNext(insert);
+			size++;
+		}
+	}
 
+	
+	/* Challenge 3:
+	 * Construct a method to insert a new node BEFORE
+	 *  the n-th node of a linked list (place), and
+	 *  add code to test it in the LinkedList class.
+	 */
+	public void insertBefore(Node insert, int place)
+	{	if(place < 1 || place > this.size()) {
+			System.out.println("ERROR - insertBefore " + place + " out of bounds!");
+			return;
+		}
+		if(place == 1) {
+			insert.setNext(head);
+			head = insert;
+			size++;
+			return;
+		}
+		int counter = 1;
+		Node temp = head;
+		while(counter < place-1) {
+			temp = temp.getNext();
+			counter++;
+		}
+		insert.setNext(temp.getNext());
+		temp.setNext(insert);
+		size++;
+	}
+
+	/* returns a String representation of the LL.
+	 * Similar to printLinkedList(), but returns a String
+	 * rather than printing it directly to the screen.
+	 * This method is called by System.out.print/println(),
+	 * allowing us to "print" a LL directly.
+	 */
+	public String toString() {
+		Node current = head;	// start at the head
+		String output = "head -> ";
+		// process/traverse/iterate over each and every node
+		while( current != null ) {
+			output = output + current.getData() + " -> ";
+			current = current.getNext(); // move to the next node
+		}
+		output += "NULL\t| size = " + size;
+		return output;
+	}
 
 
 }
